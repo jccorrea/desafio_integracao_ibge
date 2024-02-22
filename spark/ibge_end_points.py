b20fd6c4bb6d75a39b4838a249a549ee2ecbc600
Julio Cesar Correa <talktojulio@gmail.com>
2024-02-22 15:32:00 -0300

    Create ibge_end_points.py
    IBGE endpoint list

---
+++ spark/ibge_end_points.py
@@ -0,0 +1,4 @@
+#Julio Cesar Correa
+# A simple Dict with all URLs to download data
+url_endpoints = {'url_pop_mun_sp' : "https://servicodados.ibge.gov.br/api/v3/agregados/9520/periodos/-6/variaveis/93?localidades=N6[N3[35]]",
+                  'url_localidade_mun_sp' : "https://servicodados.ibge.gov.br/api/v1/localidades/estados/SP/municipios"}
